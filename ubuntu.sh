3366fa39f3d6a4adcbcb4940b41e11d1d3ee9e7e
Chao,Dou <touchao123@126.com>
2024-02-06 21:18:08 +0800

    C/C++ Dev needs

--- ubuntu.sh
+++ ubuntu.sh
@@ -28,14 +28,14 @@ read git_config_user_email
 
 # Copy dot files
 echo "Copying config files"
-cp ./editorconfig ~/.editorconfig
+# cp ./editorconfig ~/.editorconfig
 cp ./.gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
 # Updates
 echo "Running updates"
 apt-get -y update
-apt-get -y upgrade
+# apt-get -y upgrade
 
 # Curl / Wget
 echo "Installing curl"
@@ -57,93 +57,34 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
 rm ./google-chrome-stable_current_amd64.deb
 
-# Edge
-echo "Installing Edge"
-apt install -y software-properties-common apt-transport-https
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
-apt-get -y update && apt install -y microsoft-edge-dev
+# # Edge
+# echo "Installing Edge"
+# apt install -y software-properties-common apt-transport-https
+# wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+# add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+# apt-get -y update && apt install -y microsoft-edge-dev
 
-# Go
-echo "Installing GoLang"
-wget https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-tar -C /usr/local -xvzf go$GOLANG_VERSION.linux-amd64.tar.gz
-rm go$GOLANG_VERSION.linux-amd64.tar.gz
+# # Go
+# echo "Installing GoLang"
+# wget https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+# tar -C /usr/local -xvzf go$GOLANG_VERSION.linux-amd64.tar.gz
+# rm go$GOLANG_VERSION.linux-amd64.tar.gz
 
-# Node/NPM
-echo "Installing node"
-apt-get -y update && apt install nodejs -y
-
-# PHP
-echo "Installing PHP $PHP_VERSION"
-apt -y install software-properties-common
-add-apt-repository ppa:ondrej/php
-apt-get -y update && apt -y install php$PHP_VERSION
-
-# Python
-echo "Installing python"
-apt-get install python3-pip -y
-
-# MySQL
-echo "Installing NySQL and MySQL client"
-apt-get install mysql-server -y
-apt-get install mysql-client -y
-
-# Nginx
-echo "Installing Nginx"
-apt-get -y update && apt install nginx -y
-
-# VS Code
-echo "Installing VS Code"
-snap install code --classic
-
-# Jetbrains
-echo "Installing Jetbrains Toolbox"
-curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+# Docker
+echo "Docker"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker ${USER}
+newgrp docker
 
 # Dev tools
 echo "Installing dev tools"
-
-# Hugo
-apt-get -y update && apt-get install -y hugo
-
-# GoReleaser
-echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
-apt update && apt install -y goreleaser
-
-# KubeCTL
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+apt-get install build-essential
 
 # Productivity
 echo "Installing productivity apps"
-snap install simplenote
 
-# Music / Video
-echo "Installing music and video apps"
-snap install spotify
-snap install vlc
 
-# ZSH
-echo 'Installing ZSH'
-apt install -y zsh
-
-# Image / Video Optimisation
-echo "Installing image and video optimisation CLI's"
-apt-get install -y webp
-apt-get install -y optipng
-apt-get install -y jpegoptim
-apt-get install -y ffmpeg
-
-# Inject Envs
-echo 'export PATH="/usr/local/opt/php@$PHP_VERSION/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/php@$PHP_VERSION/sbin:$PATH"' >> ~/.zshrc
-echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.zshrc
-
-# Oh My ZSH (Last)
-echo "Installing Oh My ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-zsh
 
 echo ""
 echo "************************************************"
